1b0bdf3172d0fa8e688ca916e3fbda8bbd9a60e2
krishnapriya2309-cyber <krishnapriya2309@gmail.com>
2025-10-17 23:59:00 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,17 @@
-// Replace with your Firebase project config
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC1pSEnOZaqPczZj-mUwWlxjJQTv29CfS4",
+  authDomain: "weddingportraitalbum.firebaseapp.com",
+  databaseURL: "https://weddingportraitalbum-default-rtdb.firebaseio.com",
+  projectId: "weddingportraitalbum",
+  storageBucket: "weddingportraitalbum.firebasestorage.app",
+  messagingSenderId: "292563754821",
+  appId: "1:292563754821:web:42d167f3e8deb3159e282c"
 };
 
-firebase.initializeApp(firebaseConfig);
-const storage = firebase.storage();
-const db = firebase.firestore();
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
